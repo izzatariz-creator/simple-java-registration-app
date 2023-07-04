3c04ed31e5a6a86ac411e065d2270d5e7a431682
Izzat Ariz <izzatariz.2000@gmail.com>
2023-07-04 16:43:12 +0800

    first commit

---
+++ src/javaassignmentq1/JavaAssignmentQ1.java
@@ -0,0 +1,10 @@
+package javaassignmentq1;
+
+public class JavaAssignmentQ1 {
+
+    public static void main(String[] args) {
+        StaffForm sf = new StaffForm();
+        sf.setVisible(true);
+    }
+    
+}
---
+++ src/javaassignmentq1/Staff.java
@@ -0,0 +1,132 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaassignmentq1;
+
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeSupport;
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Transient;
+
+/**
+ *
+ * @author izzat
+ */
+@Entity
+@Table(name = "STAFF")
+@NamedQueries({
+    @NamedQuery(name = "Staff.findAll", query = "SELECT s FROM Staff s")
+    , @NamedQuery(name = "Staff.findById", query = "SELECT s FROM Staff s WHERE s.id = :id")
+    , @NamedQuery(name = "Staff.findByStaffid", query = "SELECT s FROM Staff s WHERE s.staffid = :staffid")
+    , @NamedQuery(name = "Staff.findByPassword", query = "SELECT s FROM Staff s WHERE s.password = :password")
+    , @NamedQuery(name = "Staff.findByName", query = "SELECT s FROM Staff s WHERE s.name = :name")})
+public class Staff implements Serializable {
+
+    @Transient
+    private PropertyChangeSupport changeSupport = new PropertyChangeSupport(this);
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "ID")
+    private Integer id;
+    @Column(name = "STAFFID")
+    private String staffid;
+    @Column(name = "PASSWORD")
+    private String password;
+    @Column(name = "NAME")
+    private String name;
+
+    public Staff() {
+    }
+
+    public Staff(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        Integer oldId = this.id;
+        this.id = id;
+        changeSupport.firePropertyChange("id", oldId, id);
+    }
+
+    public String getStaffid() {
+        return staffid;
+    }
+
+    public void setStaffid(String staffid) {
+        String oldStaffid = this.staffid;
+        this.staffid = staffid;
+        changeSupport.firePropertyChange("staffid", oldStaffid, staffid);
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        String oldPassword = this.password;
+        this.password = password;
+        changeSupport.firePropertyChange("password", oldPassword, password);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        String oldName = this.name;
+        this.name = name;
+        changeSupport.firePropertyChange("name", oldName, name);
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Staff)) {
+            return false;
+        }
+        Staff other = (Staff) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "javaassignmentq1.Staff[ id=" + id + " ]";
+    }
+
+    public void addPropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.addPropertyChangeListener(listener);
+    }
+
+    public void removePropertyChangeListener(PropertyChangeListener listener) {
+        changeSupport.removePropertyChangeListener(listener);
+    }
+    
+}
